7e1ff2778af1a66db53cf2c5a4165d96281ac1b7
Mark Railton <markjasonrailton@gmail.com>
2026-03-28 18:37:41 +0000

    fixes

--- app/__init__.py
+++ app/__init__.py
@@ -38,7 +38,7 @@ def create_app():
     app.register_blueprint(settings_bp)
 
     with app.app_context():
-        db.create_all()
+        db.create_all(checkfirst=True)
         _ensure_admin()
         init_scheduler(app)
 
